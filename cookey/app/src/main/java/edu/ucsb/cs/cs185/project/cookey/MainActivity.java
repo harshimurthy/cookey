4c210b9cb285e5f20b15a5f5bb7b1ed3f6088920
harshithachidanand <harshithac94@gmail.com>
2016-06-03 14:16:51 -0700

    navigation 4 now

--- cookey/app/src/main/java/edu/ucsb/cs/cs185/project/cookey/MainActivity.java
+++ cookey/app/src/main/java/edu/ucsb/cs/cs185/project/cookey/MainActivity.java
@@ -91,10 +91,6 @@ public class MainActivity extends AppCompatActivity
             Intent intent=new Intent(MainActivity.this, SettingsPage.class);
             startActivity(intent);
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
